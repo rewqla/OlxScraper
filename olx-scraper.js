15d04ba83f7eff8031a82c16891f1182fe6865f4
rewqla <desares526@gmail.com>
2024-01-06 15:50:27 +0200

    Add olx scrapper to retrieve data from url

--- olx-scraper.js
+++ olx-scraper.js
@@ -1,4 +1,32 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const url = 'https://www.olx.ro/imobiliare/apartamente-garsoniere-de-vanzare/baia-mare/?page=1';
+const url = 'https://www.olx.ua/uk/hobbi-otdyh-i-sport/knigi-zhurnaly/q-%D1%87%D0%B5%D1%82%D0%B2%D0%B5%D1%80%D1%82%D0%B5-%D0%BA%D1%80%D0%B8%D0%BB%D0%BE/';
+const url2 = 'https://www.olx.ua/uk/hobbi-otdyh-i-sport/knigi-zhurnaly/q-%D1%81%D1%82%D1%80%D0%B0%D1%85-%D0%BC%D1%83%D0%B4%D1%80%D0%B5%D1%86%D1%8F/';
+
+axios(url2)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html, { decodeEntities: false });
+        const gridContainer = $('div[data-cy="l-card"]');
+        const goodsData = [];
+
+        gridContainer.each(function () {
+            const name = $(this).find('a > div > div > div > div.css-u2ayx9 > h6').text();
+            const price = $(this).find('a > div > div > div> div.css-u2ayx9 > p').text().trim();
+            const condition = $(this).find('a > div > div > div > div.css-iqvdlb > span > span > span').text();
+            const location = $(this).find('a > div > div > div > div.css-odp1qd > p').text();
+            // const date = $(this).find('.stats-table__main-stat').text();
+
+            goodsData.push({
+                name,
+                price,
+                condition,
+                location,
+                // date,
+            });
+        });
+
+        console.log(goodsData);
+    })
+    .catch(console.error);
